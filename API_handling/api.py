caa5cfc6321c4d97ed26baa8ada23705d2240df9
gulugulu2042 <gokul26.09.2004@gmail.com>
2025-04-02 16:27:33 +0530

    add api code

---
+++ API_handling/api.py
@@ -0,0 +1,23 @@
+import requests
+
+URL = "https://my-json-server.typicode.com/Gokularajan-R/Intern-tasks/content"
+
+response = requests.get(URL)
+print(response.json())
+
+response = requests.get(f"{URL}/31")
+print(response.json())
+
+#CHANGES WILL NOT BE REFLECTED
+#------------------------------------------------
+new_post = {"id": 16, "name": "Yuva"}
+response = requests.post(URL, json=new_post)
+print(response.json())
+
+updated_post = {"id": 31, "name": "Updated Post"}
+response = requests.put(f"{URL}/31", json=updated_post)
+print(response.json())
+
+response = requests.delete(f"{URL}/31")
+print("DELETE Response:", response.status_code)
+#------------------------------------------------
