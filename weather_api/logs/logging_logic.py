b54db3836f1e05e7ba756a6290d5fd896792a70f
gulugulu2042 <gokul26.09.2004@gmail.com>
2025-06-13 12:08:35 +0530

    double logging fix attempt

--- weather_api/logs/logging_logic.py
+++ weather_api/logs/logging_logic.py
@@ -1,11 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
-import os
 
 def setup_logger(name):
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
@@ -14,15 +10,10 @@ def setup_logger(name):
         maxBytes=1024*1024,
         backupCount=5
     )
-    #console_handler = logging.StreamHandler()
 
-    log_format = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
+    log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(log_format)
-    #console_handler.setFormatter(log_format)
-
     logger.addHandler(file_handler)
-    #logger.addHandler(console_handler)
+    logger.propagate = False
 
     return logger
